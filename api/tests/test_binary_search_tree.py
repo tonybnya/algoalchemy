5876ff18dc428f3a66809bd0c4d5d6109eada105
tonybnya <nya.tony2010@gmail.com>
2025-05-13 16:34:04 +0100

    feat: test insert at multiple levels

--- api/tests/test_binary_search_tree.py
+++ api/tests/test_binary_search_tree.py
@@ -16,39 +16,50 @@ def test_insert_root():
     """
     Test insertion at the root.
     """
-    tree = BST()
-    tree.insert(10)
-    assert tree.root is not None
-    assert tree.root.data == 10
+    bst = BST()
+    bst.insert(10)
+    assert bst.root is not None
+    assert bst.root.data == 10
 
 
-# def test_insert_left_and_right_children():
-#     tree = BST()
-#     tree.insert(10)
-#     tree.insert(5)
-#     tree.insert(15)
-#
-#     assert tree.root.left.data == 5
-#     assert tree.root.right.data == 15
-#
-#
-# def test_recursive_insert_multiple_levels():
-#     tree = BST()
-#     values = [10, 5, 15, 2, 7, 12, 20]
-#     for val in values:
-#         tree.insert(val)
-#
-#     assert tree.root.left.left.data == 2
-#     assert tree.root.left.right.data == 7
-#     assert tree.root.right.left.data == 12
-#     assert tree.root.right.right.data == 20
-#
-#
+def test_insert_left_and_right_children():
+    """
+    Test insertion at left and right.
+    """
+    bst = BST()
+    bst.insert(10)
+    bst.insert(5)
+    bst.insert(15)
+
+    assert bst.root.left.data == 5
+    assert bst.root.right.data == 15
+
+
+def test_recursive_insert_multiple_levels():
+    """
+    Test insertion at multiple levels.
+    """
+    bst = BST()
+    values = [10, 5, 15, 2, 7, 12, 20]
+    for val in values:
+        bst.insert(val)
+
+    assert bst.root.data == 10
+
+    assert bst.root.left.data == 5
+    assert bst.root.left.left.data == 2
+    assert bst.root.left.right.data == 7
+
+    assert bst.root.right.data == 15
+    assert bst.root.right.right.data == 20
+    assert bst.root.right.left.data == 12
+
+
 # def test_no_duplicates_inserted():
-#     tree = BST()
-#     tree.insert(10)
-#     tree.insert(5)
-#     tree.insert(10)  # duplicate
-#     assert tree.root.data == 10
-#     assert tree.root.left.data == 5
-#     assert tree.root.right is None
+#     bst = BST()
+#     bst.insert(10)
+#     bst.insert(5)
+#     bst.insert(10)  # duplicate
+#     assert bst.root.data == 10
+#     assert bst.root.left.data == 5
+#     assert bst.root.right is None
